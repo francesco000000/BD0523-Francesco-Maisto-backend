5b9c6310f34fd6c598aa4911a8fa24101d6c0ae4
Francesco-Maisto <franco.maisto@outlook.com>
2024-01-08 15:57:22 +0100

    esercizio

---
+++ src/it/epicode/week1/day1/PrimaClasse.java
@@ -0,0 +1,5 @@
+package it.epicode.week1.day1;
+
+public class PrimaClasse {
+
+}
---
+++ src/it/epicode/week1/day1/esercizio3.java
@@ -0,0 +1,36 @@
+package it.epicode.week1.day1;
+
+import java.util.Scanner;
+
+public class esercizio3 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("inserisci la base");
+        double base =scan.nextDouble();
+        System.out.println("inserisci l'altezza");
+        double altezza =scan.nextDouble();
+        double perimetro = perimetroRettangolo(base,altezza);
+        System.out.println("il perimentro è:" + perimetro);
+        System.out.println( pariDispari(13));
+        System.out.println("l'area del triangolo è:" + areaTriangolo(2.2,3.7));
+
+    }
+    public static double perimetroRettangolo (double base,double altezza){
+        double perimetro = (base + altezza) * 2;
+        return perimetro;
+
+    }
+    public static int pariDispari (int numero) {
+        if (numero % 2 == 0) {
+            return 0;
+        }
+         else {
+            return 1;
+        }
+   }
+    public static double areaTriangolo (double base,double altezza){
+        double perimetro = (base * altezza) / 2;
+        return perimetro;
+
+    }
+}
